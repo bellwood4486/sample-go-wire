5f6db1978cdafd35ef10d767df25bf4faa2ed133
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2020-03-28 23:10:17 +0900

    feat: Change the injectr signature

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 
 type Message string
 
-func NewMessage() Message {
-	return Message("Hi there!")
+func NewMessage(phrase string) Message {
+	return Message(phrase)
 }
 
 type Greeter struct {
@@ -50,7 +50,7 @@ func (e Event) Start() {
 }
 
 func main() {
-	e, err := InitializeEvent()
+	e, err := InitializeEvent("hi there!")
 	if err != nil {
 		fmt.Printf("failed to create event: %s\n", err)
 		os.Exit(2)
